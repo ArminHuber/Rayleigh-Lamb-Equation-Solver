f339d7adc20f49a3322db711c99556a6c9c27c08
Armin Huber <43004587+ArminHuber@users.noreply.github.com>
2023-04-26 09:02:02 +0200

    Add files via upload

--- RayleighLambEquationSolver.m
+++ RayleighLambEquationSolver.m
@@ -1,5 +1,5 @@
 %% Rayleigh-Lamb Equation Solver
-% Update 2023/04/25
+% Update 2023/04/26
 % written by: Armin Huber, armin.huber@dlr.de
 % -------------------------------------------------------------------------
 % Calculate Lamb wave dispersion diagrams for a free isotropic plate by
@@ -24,20 +24,20 @@ Half = Thickness/2e3;
 Frequency = 0:FrequencyLimit/FrequencySteps:FrequencyLimit; % generate a range of frequencies
 PhaseVelocity = (0:PhaseVelocityLimit/PhaseVelocitySteps:PhaseVelocityLimit)*1e3; % generate a range of phase velocities
 AngularFrequency = 2*pi*Frequency*1e3;
-k2 = (AngularFrequency./repmat(PhaseVelocity',1,length(AngularFrequency))).^2; % wavenumber^2 of Lamb waves
-kL2 = repmat((AngularFrequency/Material.LongitudinalVelocity).^2,length(PhaseVelocity),1); % wavenumber^2 of longitudinal bulk waves
-kT2 = repmat((AngularFrequency/Material.TransverseVelocity).^2,length(PhaseVelocity),1); % wavenumber^2 of transverse bulk waves
+k2 = (AngularFrequency./PhaseVelocity').^2; % wavenumber^2 of Lamb waves
+kL2 = (AngularFrequency/Material.LongitudinalVelocity).^2; % wavenumber^2 of longitudinal bulk waves
+kT2 = (AngularFrequency/Material.TransverseVelocity).^2; % wavenumber^2 of transverse bulk waves
 x = sqrt(kL2-k2); % out-of-plane wavenumber component of longitudinal bulk waves
 y = sqrt(kT2-k2); % out-of-plane wavenumber component of transverse bulk waves
-a1 = (y.^2-k2).^2;
+a1 = (y.^2-k2).^2./y;
 a2 = 4*k2.*x;
 a3 = tan(x*Half);
 a4 = tan(y*Half);
-S = abs(a1./a3./y+a2./a4); % Rayleigh-Lamb equation for symmetric modes (absolute value)
-A = abs(a1.*a3./y+a2.*a4); % Rayleigh-Lamb equation for antisymmetric modes (absolute value)
+S = abs(a1./a3+a2./a4); % Rayleigh-Lamb equation for symmetric modes (absolute value)
+A = abs(a1.*a3+a2.*a4); % Rayleigh-Lamb equation for antisymmetric modes (absolute value)
 
 %% dispersion diagrams
-f = figure('Name','Symmetric mode dispersion diagram','Units','normalized','Toolbar','none','OuterPosition',[0 0 1 1],'color','w'); %#ok<*UNRCH> 
+f = figure('Name','Symmetric mode dispersion diagram','Units','normalized','Toolbar','none','OuterPosition',[0 0 1 1],'color','w');
 datacursormode on
 imagesc(Frequency,PhaseVelocity/1e3,20*log10(S)) % Rayleigh-Lamb equation amplitude in dB
 colormap(gray(1024));
@@ -83,6 +83,6 @@ d = datacursormode(f);
 d.Interpreter = 'latex';
 d.UpdateFcn = @MyCursor;
 
-function output_txt = MyCursor(~,event_obj) %#ok<*DEFNU> 
+function output_txt = MyCursor(~,event_obj)
     output_txt = {['$f$: \textbf{',num2str(event_obj.Position(1),6),'} kHz'] ['$c_{\mathrm{p}}$: \textbf{',num2str(event_obj.Position(2),6),'} m/ms']};
 end
--- RayleighLambEquationSolver.py
+++ RayleighLambEquationSolver.py
@@ -1,6 +1,6 @@
 """
 Rayleigh-Lamb Equation Solver
-Update 2023/04/25
+Update 2023/04/26
 written by: Armin Huber, armin.huber@dlr.de
 -------------------------------------------------------------------------------
 Calculate Lamb wave dispersion diagrams for a free isotropic plate by
@@ -29,17 +29,17 @@ Half = Thickness/2e3
 Frequency = np.arange(0,FrequencyLimit+FrequencyLimit/FrequencySteps,FrequencyLimit/FrequencySteps);Frequency[0] = 1 # generate a range of frequencies
 PhaseVelocity = np.arange(0,PhaseVelocityLimit*1e3+PhaseVelocityLimit*1e3/PhaseVelocitySteps,PhaseVelocityLimit*1e3/PhaseVelocitySteps);PhaseVelocity[0] = 1 # generate a range of phase velocities
 AngularFrequency = 2*np.pi*Frequency*1e3
-k2 = (AngularFrequency/np.matrix.transpose(np.tile(PhaseVelocity,(AngularFrequency.size,1))))**2 # wavenumber^2 of Lamb waves
-kL2 = np.tile((AngularFrequency/MaterialLongitudinalVelocity)**2,(PhaseVelocity.size,1)) # wavenumber^2 of longitudinal bulk waves
-kT2 = np.tile((AngularFrequency/MaterialTransverseVelocity)**2,(PhaseVelocity.size,1)) # wavenumber^2 of transverse bulk waves
+k2 = (AngularFrequency/PhaseVelocity[:,np.newaxis])**2 # wavenumber^2 of Lamb waves
+kL2 = (AngularFrequency/MaterialLongitudinalVelocity)**2 # wavenumber^2 of longitudinal bulk waves
+kT2 = (AngularFrequency/MaterialTransverseVelocity)**2 # wavenumber^2 of transverse bulk waves
 x = np.lib.scimath.sqrt(kL2-k2) # out-of-plane wavenumber component of longitudinal bulk waves
 y = np.lib.scimath.sqrt(kT2-k2) # out-of-plane wavenumber component of transverse bulk waves
-a1 = (y**2-k2)**2
+a1 = (y**2-k2)**2/y
 a2 = 4*k2*x
 a3 = np.tan(x*Half)
 a4 = np.tan(y*Half)
-S = np.abs(a1/a3/y+a2/a4) # Rayleigh-Lamb equation for symmetric modes (absolute value)
-A = np.abs(a1*a3/y+a2*a4) # Rayleigh-Lamb equation for antisymmetric modes (absolute value)
+S = np.abs(a1/a3+a2/a4) # Rayleigh-Lamb equation for symmetric modes (absolute value)
+A = np.abs(a1*a3+a2*a4) # Rayleigh-Lamb equation for antisymmetric modes (absolute value)
 
 # %% dispersion diagrams
 plt.figure(figsize=[16,10])
